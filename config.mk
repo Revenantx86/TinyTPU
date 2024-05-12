088924ef78e3f71f764d25a2774e905d978fb814
Revenantx86 <refikyalcin@sabanciuniv.edu>
2024-05-11 21:09:04 -0400

    config commit

---
+++ config.mk
@@ -0,0 +1,15 @@
+export DESIGN_NICKNAME = TinyTPU
+export DESIGN_NAME = top
+export PLATFORM    = sky130hd
+
+export VERILOG_FILES = $(sort $(wildcard ./designs/src/$(DESIGN_NICKNAME)/src/*.v))
+export SDC_FILE      = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
+
+export PLACE_PINS_ARGS = -min_distance 4 -min_distance_in_tracks
+
+export CORE_UTILIZATION = 20
+export CORE_ASPECT_RATIO = 1
+export CORE_MARGIN = 2
+
+export PLACE_DENSITY = 0.6
+export TNS_END_PERCENT = 100
