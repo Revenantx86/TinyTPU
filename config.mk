78b8825bc14cddb6e372d7a08761df61aa0b8dac
Revenantx86 <refikyalcin@sabanciuniv.edu>
2024-05-11 21:16:09 -0400

    file format change

--- config.mk
+++ config.mk
@@ -2,7 +2,8 @@ export DESIGN_NICKNAME = TinyTPU
 export DESIGN_NAME = top
 export PLATFORM    = sky130hd
 
-export VERILOG_FILES = $(sort $(wildcard ./designs/src/$(DESIGN_NICKNAME)/src/*.v))
+export VERILOG_FILES = ./designs/TinyTPU/src/*.v
+#export VERILOG_FILES = $(sort $(wildcard ./designs/src/$(DESIGN_NICKNAME)/src/*.v))
 export SDC_FILE      = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
 
 export PLACE_PINS_ARGS = -min_distance 4 -min_distance_in_tracks
